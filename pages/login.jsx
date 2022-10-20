e7d60c0f2ae27baa5a6294bdbc6322574120dd7d
partisi <erolmb@gmail.com>
2022-10-19 20:29:17 -0400

    user auth google

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { getSession, signIn } from "next-auth/react";
+import React from "react";
 
-export default function Login() {
-    return (
-        <div>
-            <h1>login</h1>
-        </div>
-    )
+export default function login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <button
+        onClick={() => {
+          signIn();
+        }}
+      >
+        Login
+      </button>
+    </div>
+  );
 }
+
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  if (!!session) {
+    return {
+      props: { session },
+      redirect: { destination: "/" },
+    };
+  } else {
+    return { props: { session } };
+  }
+};
